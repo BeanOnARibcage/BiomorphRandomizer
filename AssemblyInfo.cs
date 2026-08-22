b0d7ccfa8d785c740a01e3b7b976905182b3b369
BeanOnARibcage <bean.ribcage@gmail.com>
2026-08-21 23:44:41 -0500

    updated version number

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using MelonLoader;
 using BiomorphRandomizer;
 
-[assembly: MelonInfo(typeof(Randomizer), "Biomorph Randomizer", "0.1.0", "BeanOnARibcage")]
+[assembly: MelonInfo(typeof(Randomizer), "Biomorph Randomizer", "0.1.1", "BeanOnARibcage")]
 [assembly: MelonGame("Lucid Dreams Studio", "BIOMORPH")]
